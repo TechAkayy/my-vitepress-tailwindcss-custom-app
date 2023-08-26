b17049d01a2b7529f4fd006bb9b19f9759dbc8b7
Akayy <techakayy@gmail.com>
2023-08-26 14:30:13 +1000

    Minimal

--- src/components/SampleCard.vue
+++ src/components/SampleCard.vue
@@ -1,10 +1,12 @@
+<script setup lang="ts">
+  // TODO:  Need to extend this component to add cart.
+</script>
+
 <template>
   <div class="flex flex-col items-center p-4">
     <ChildCard />
-    <button class="rounded-full bg-blue-600 text-white p-4">
-      Quick Start
-      <!--  -->
-    </button>
+    <BaseButton />
+    <button class="rounded-full bg-blue-600 text-white p-4">Quick Start</button>
   </div>
 </template>
 <style scoped></style>
